afb1e255950e6358ca78c23a392b7e545369b0eb
stellaruniversex <stellaruniversexmpls@gmail.com>
2020-02-28 21:41:02 +0200

    Added form template

---
+++ BaigiamasisSmuilys/MyForm.h
@@ -0,0 +1,218 @@
+﻿#pragma once
+
+namespace BaigiamasisSmuilys {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Summary for MyForm
+	/// </summary>
+	public ref class MyForm : public System::Windows::Forms::Form
+	{
+	public:
+		MyForm(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: Add the constructor code here
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		~MyForm()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::Label^  label1;
+	protected:
+	private: System::Windows::Forms::Label^  label2;
+	private: System::Windows::Forms::Label^  label3;
+	private: System::Windows::Forms::Label^  label4;
+	private: System::Windows::Forms::Label^  label5;
+	private: System::Windows::Forms::Label^  label6;
+	private: System::Windows::Forms::TextBox^  textBox1;
+	private: System::Windows::Forms::TextBox^  textBox2;
+	private: System::Windows::Forms::TextBox^  textBox3;
+	private: System::Windows::Forms::TextBox^  textBox4;
+	private: System::Windows::Forms::TextBox^  textBox5;
+	private: System::Windows::Forms::Button^  button1;
+
+	private:
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->label3 = (gcnew System::Windows::Forms::Label());
+			this->label4 = (gcnew System::Windows::Forms::Label());
+			this->label5 = (gcnew System::Windows::Forms::Label());
+			this->label6 = (gcnew System::Windows::Forms::Label());
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox3 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox4 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox5 = (gcnew System::Windows::Forms::TextBox());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->SuspendLayout();
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Font = (gcnew System::Drawing::Font(L"Consolas", 14.25F));
+			this->label1->Location = System::Drawing::Point(12, 80);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(120, 22);
+			this->label1->TabIndex = 0;
+			this->label1->Text = L"Pavadinimas";
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Font = (gcnew System::Drawing::Font(L"Consolas", 20.25F));
+			this->label2->Location = System::Drawing::Point(103, 9);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(255, 32);
+			this->label2->TabIndex = 1;
+			this->label2->Text = L"Prekės įterpimas";
+			// 
+			// label3
+			// 
+			this->label3->AutoSize = true;
+			this->label3->Font = (gcnew System::Drawing::Font(L"Consolas", 14.25F));
+			this->label3->Location = System::Drawing::Point(12, 159);
+			this->label3->Name = L"label3";
+			this->label3->Size = System::Drawing::Size(60, 22);
+			this->label3->TabIndex = 2;
+			this->label3->Text = L"Kaina";
+			// 
+			// label4
+			// 
+			this->label4->AutoSize = true;
+			this->label4->Font = (gcnew System::Drawing::Font(L"Consolas", 14.25F));
+			this->label4->Location = System::Drawing::Point(12, 202);
+			this->label4->Name = L"label4";
+			this->label4->Size = System::Drawing::Size(150, 22);
+			this->label4->TabIndex = 3;
+			this->label4->Text = L"Galiojimo data";
+			// 
+			// label5
+			// 
+			this->label5->AutoSize = true;
+			this->label5->Font = (gcnew System::Drawing::Font(L"Consolas", 14.25F));
+			this->label5->Location = System::Drawing::Point(12, 244);
+			this->label5->Name = L"label5";
+			this->label5->Size = System::Drawing::Size(70, 22);
+			this->label5->TabIndex = 4;
+			this->label5->Text = L"Kiekis";
+			this->label5->Click += gcnew System::EventHandler(this, &MyForm::label5_Click);
+			// 
+			// label6
+			// 
+			this->label6->AutoSize = true;
+			this->label6->Font = (gcnew System::Drawing::Font(L"Consolas", 14.25F));
+			this->label6->Location = System::Drawing::Point(12, 118);
+			this->label6->Name = L"label6";
+			this->label6->Size = System::Drawing::Size(110, 22);
+			this->label6->TabIndex = 5;
+			this->label6->Text = L"Gamintojas";
+			// 
+			// textBox1
+			// 
+			this->textBox1->Font = (gcnew System::Drawing::Font(L"Consolas", 14.25F));
+			this->textBox1->Location = System::Drawing::Point(232, 72);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(203, 30);
+			this->textBox1->TabIndex = 6;
+			// 
+			// textBox2
+			// 
+			this->textBox2->Font = (gcnew System::Drawing::Font(L"Consolas", 14.25F));
+			this->textBox2->Location = System::Drawing::Point(232, 115);
+			this->textBox2->Name = L"textBox2";
+			this->textBox2->Size = System::Drawing::Size(203, 30);
+			this->textBox2->TabIndex = 7;
+			// 
+			// textBox3
+			// 
+			this->textBox3->Font = (gcnew System::Drawing::Font(L"Consolas", 14.25F));
+			this->textBox3->Location = System::Drawing::Point(232, 156);
+			this->textBox3->Name = L"textBox3";
+			this->textBox3->Size = System::Drawing::Size(203, 30);
+			this->textBox3->TabIndex = 8;
+			// 
+			// textBox4
+			// 
+			this->textBox4->Font = (gcnew System::Drawing::Font(L"Consolas", 14.25F));
+			this->textBox4->Location = System::Drawing::Point(232, 199);
+			this->textBox4->Name = L"textBox4";
+			this->textBox4->Size = System::Drawing::Size(203, 30);
+			this->textBox4->TabIndex = 9;
+			// 
+			// textBox5
+			// 
+			this->textBox5->Font = (gcnew System::Drawing::Font(L"Consolas", 14.25F));
+			this->textBox5->Location = System::Drawing::Point(232, 241);
+			this->textBox5->Name = L"textBox5";
+			this->textBox5->Size = System::Drawing::Size(203, 30);
+			this->textBox5->TabIndex = 10;
+			// 
+			// button1
+			// 
+			this->button1->Font = (gcnew System::Drawing::Font(L"Consolas", 24.25F));
+			this->button1->Location = System::Drawing::Point(144, 295);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(174, 54);
+			this->button1->TabIndex = 11;
+			this->button1->Text = L"Įterpti";
+			this->button1->UseVisualStyleBackColor = true;
+			// 
+			// MyForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(32)), static_cast<System::Int32>(static_cast<System::Byte>(192)),
+				static_cast<System::Int32>(static_cast<System::Byte>(0)));
+			this->ClientSize = System::Drawing::Size(484, 461);
+			this->Controls->Add(this->button1);
+			this->Controls->Add(this->textBox5);
+			this->Controls->Add(this->textBox4);
+			this->Controls->Add(this->textBox3);
+			this->Controls->Add(this->textBox2);
+			this->Controls->Add(this->textBox1);
+			this->Controls->Add(this->label6);
+			this->Controls->Add(this->label5);
+			this->Controls->Add(this->label4);
+			this->Controls->Add(this->label3);
+			this->Controls->Add(this->label2);
+			this->Controls->Add(this->label1);
+			this->Name = L"MyForm";
+			this->Text = L"MyForm";
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
+		}
+#pragma endregion
+	private: System::Void label5_Click(System::Object^  sender, System::EventArgs^  e) {
+	}
+};
+}
